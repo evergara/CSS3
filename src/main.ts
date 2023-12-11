29291f844cbf2ac5e89177ae127cd89d348dd4d2
Emerson vergara <emerson.vergara>
2023-12-11 11:15:35 -0500

    Commit - configuracion

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,12 @@ import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-
-import { appRoutes } from './app/app.route';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideRouter(appRoutes),]
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
 })
   .catch(err => console.error(err));
